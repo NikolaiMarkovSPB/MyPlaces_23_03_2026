67b5b51975c1e169eaf1b47b55493f2ec0327d5b
Nikolay <mmaarkow@gmail.com>
2026-03-23 22:58:06 +0300

    Initial Commit

---
+++ MyPlaces_23_03_2026Tests/MyPlaces_23_03_2026Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlaces_23_03_2026Tests.swift
+//  MyPlaces_23_03_2026Tests
+//
+//  Created by Николай Марков on 23.03.2026.
+//
+
+import Testing
+@testable import MyPlaces_23_03_2026
+
+struct MyPlaces_23_03_2026Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
